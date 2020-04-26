5003714ddfb07257098ffd6a302e0cec52ce521f
Lucas Fernandes <lucasmatdlx@hotmail.com>
2020-04-26 08:44:12 -0300

    Update tweet_bot.py

--- twitter_Bot_twitch 26-04-2020/tweet_bot.py
+++ twitter_Bot_twitch 26-04-2020/tweet_bot.py
@@ -1,15 +1,18 @@
 import tweepy
 import time
+from Parametros import RC
 
-class twitter_bot():
+class twitterBot():
+
     def __init__(self):
         self.consumer_key = ""
         self.consumer_secret = ""
         self.access_token = ""
         self.access_token_secret = ""
-        self.Tweet_Inicial = "Olá, sou o HaroldoBOT https://www.twitch.tv/lucasfernsilva"
-        self.api= None
-        self.array_rt= []
+        self.Tweet_Inicial = "https://www.twitch.tv/lucasfernsilva"
+        self.api = None
+        self.array_rt = []
+
     def api_key(self):
         # Inicia com as credenciais
         auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
@@ -22,20 +25,23 @@ class twitter_bot():
 
     def curte_tweet(self):
         public_tweets = self.api.home_timeline()
-        filtro = "Olá, sou o HaroldoBOT1"
+
 # Percorre pelos tweets e retweeta o que possui a palavra especifica
         for tweet in public_tweets:
-            if filtro in tweet.text :
+            if RC.filtro in tweet.text :
                 print(tweet.id)
                 self.api.retweet(tweet.id)
                 self.array_rt.append(tweet.text)
+                if RC.filtro is None:
+                    return "Não há filtro selecionado!"
+                elif tweet.text is None :
+                    return "Não há tweets na sua timeline!"
 
         print(self.array_rt)
 
 
-
-
-a = twitter_bot()
+a = twitterBot()
 
 a.api_key()
+time.sleep(10)
 a.curte_tweet()
